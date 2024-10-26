30e9c01cbbf503c3f5701b29b6304f69c19879d9
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 21:53:14 +0300

    create test3

--- src/main/test/java/com/netflix/LoginTest.java
+++ src/main/test/java/com/netflix/LoginTest.java
@@ -8,22 +8,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginTest {
 
     @Test
-    public void test1(){WebDriver webDriver = new ChromeDriver();
+    public void test1() {
+        WebDriver webDriver = new ChromeDriver();
         webDriver.navigate().to("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.clickLoginButton();
-        Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD,loginPage.getActualMessageWithPassword());
-        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD,loginPage.getActualMessageWithLogin());
+        Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD, loginPage.getActualMessageWithPassword());
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD, loginPage.getActualMessageWithLogin());
     }
 
     @Test
-    public void test2(){WebDriver webDriver = new ChromeDriver();
+    public void test2() {
+        WebDriver webDriver = new ChromeDriver();
         webDriver.navigate().to("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
         loginPage.fillLoginField("12345@mail.ru");
         loginPage.clickLoginButton();
-        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD,loginPage.getActualMessageWithLogin());
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD, loginPage.getActualMessageWithLogin());
     }
 
+    @Test
+    public void test3() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.fillPasswordField("12345qwerty");
+        loginPage.clickLoginButton();
+        Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD, loginPage.getActualMessageWithPassword());
+    }
 
 }
