451628cd14c422034c6fa9718c44360852f4a112
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 21:51:58 +0300

    create test2

--- src/main/test/java/com/netflix/LoginTest.java
+++ src/main/test/java/com/netflix/LoginTest.java
@@ -15,4 +15,15 @@ public class LoginTest {
         Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD,loginPage.getActualMessageWithPassword());
         Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD,loginPage.getActualMessageWithLogin());
     }
+
+    @Test
+    public void test2(){WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.fillLoginField("12345@mail.ru");
+        loginPage.clickLoginButton();
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD,loginPage.getActualMessageWithLogin());
+    }
+
+
 }
