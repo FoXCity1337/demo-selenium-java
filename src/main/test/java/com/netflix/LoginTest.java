c53af127fd8b6c373b3a3e8fdf5c5f748a5f8e92
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 21:51:11 +0300

    create test1

--- src/main/test/java/com/netflix/LoginTest.java
+++ src/main/test/java/com/netflix/LoginTest.java
@@ -1,5 +1,6 @@
 package com.netflix;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,5 +11,8 @@ public class LoginTest {
     public void test1(){WebDriver webDriver = new ChromeDriver();
         webDriver.navigate().to("https://www.netflix.com/login");
         LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.clickLoginButton();
+        Assertions.assertEquals(LoginMessage.EMPTY_LOGIN_FIELD,loginPage.getActualMessageWithPassword());
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD,loginPage.getActualMessageWithLogin());
     }
 }
