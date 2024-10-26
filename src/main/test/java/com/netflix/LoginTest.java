1d47098322b124c19612366f2236da4cec8face7
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 21:17:41 +0300

    create constructor of LoginPage class

--- src/main/test/java/com/netflix/LoginTest.java
+++ src/main/test/java/com/netflix/LoginTest.java
@@ -1,4 +1,14 @@
 package com.netflix;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+
+    @Test
+    public void test1(){WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://www.netflix.com/login");
+        LoginPage loginPage = new LoginPage(webDriver);
+    }
 }
