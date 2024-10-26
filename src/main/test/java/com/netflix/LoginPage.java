cb481167f98fff2dfc6ee888ea5be7c6f27438ff
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 21:23:11 +0300

    create fillLoginField method

--- src/main/test/java/com/netflix/LoginPage.java
+++ src/main/test/java/com/netflix/LoginPage.java
@@ -1,5 +1,18 @@
 package com.netflix;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class LoginPage {
+    private final WebDriver webDriver;
+    public LoginPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void fillLoginField(String login){
+        By loginBy = By.xpath(LoginXpath.LOGIN_FIELD);
+        WebElement loginWebElement = webDriver.findElement(loginBy);
+        loginWebElement.sendKeys(login);
+    }
 }
