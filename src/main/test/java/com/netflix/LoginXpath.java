e39ec62fdf8931d7103ee6b5f588e09a44cc7aae
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-26 20:52:40 +0300

    create LoginXpath class

---
+++ src/main/test/java/com/netflix/LoginXpath.java
@@ -0,0 +1,7 @@
+package com.netflix;
+
+public class LoginXpath {
+    public static final String LOGIN_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
+    public static final String PASSWORD_FIELD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div/input";
+    public static final String LOGIN_BUTTON = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
+}
