09f4f540c632d21a6c749220dc629e55ed2bed4e
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-27 17:34:04 +0300

    Added constant fields in LoginXPath class

--- src/test/java/mall/LoginXpath.java
+++ src/test/java/mall/LoginXpath.java
@@ -1,4 +1,8 @@
 package mall;
 
 public class LoginXpath {
+    public static final String PHONE_NUMBER_FIELD_XPATH = "/html/body/div[1]/div/div/div[1]/form/div[1]/input";
+    public static final String PASSWORD_FIELD_XPATH = "/html/body/div[1]/div/div/div[1]/form/div[2]/input";
+    public static final String LOGIN_BUTTON_XPATH = "/html/body/div[1]/div/div/div[1]/form/button";
+    public static final String LOGIN_ERROR_XPATH = "/html/body/div[1]/div/div/div[1]/form/div[3]";
 }
