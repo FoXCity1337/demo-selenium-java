bb51bd1d506f35b1b88e0a3bcfe80f617c6e383b
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-27 17:40:28 +0300

    Added fillPhoneNumberField and fillPasswordField methods

--- src/test/java/mall/LoginPage.java
+++ src/test/java/mall/LoginPage.java
@@ -1,4 +1,25 @@
 package mall;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+    private WebDriver webDriver;
+
+    public LoginPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void fillPhoneNumberField(String number){
+        By phoneNumberBy = By.xpath(LoginXpath.PHONE_NUMBER_FIELD_XPATH);
+        WebElement phoneNumberWebDriver = webDriver.findElement(phoneNumberBy);
+        phoneNumberWebDriver.sendKeys(number);
+    }
+
+    public void fillPasswordField(String password){
+        By paswordBy = By.xpath(LoginXpath.PHONE_NUMBER_FIELD_XPATH);
+        WebElement phoneNumberWebDriver = webDriver.findElement(paswordBy);
+        phoneNumberWebDriver.sendKeys(password);
+    }
 }
