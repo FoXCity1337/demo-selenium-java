e62cffd980e0a1e1128ea02c6273ebf4b5a6ad70
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-27 17:22:23 +0300

    Added constant fields in LoginMessage class

--- src/test/java/mall/LoginMessage.java
+++ src/test/java/mall/LoginMessage.java
@@ -1,4 +1,12 @@
 package mall;
 
 public class LoginMessage {
+    public static final String EMPTY_FIELDS_MESSAGE = "Поле \"Номер телефона\" обязательно для заполнения.\n" +
+            "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String EMPTY_PASSWORD_FIELD_MESSAGE =
+            "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String EMPTY_PHONE_NUMBER_FIELD_MESSAGE =
+            "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр " +
+                    "(первая из которых не 0)";
+    public static final String WRONG_EMAIL_OR_PASSWORD_MESSAGE = "Неверный логин или пароль";
 }
