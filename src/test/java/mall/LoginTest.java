dcf8503098602b6852d6536296a77db27c678cc1
Arseni Aleksandrovich <theintosky@gmail.com>
2024-10-27 18:02:14 +0300

    Added Test3 for empty phone number field

--- src/test/java/mall/LoginTest.java
+++ src/test/java/mall/LoginTest.java
@@ -24,4 +24,14 @@ public class LoginTest {
         loginPage.clickLoginButton();
         Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_FIELD_MESSAGE,loginPage.getErrorMessage());
     }
+
+    @Test
+    public void Test3() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.fillPasswordField("12345qwerty");
+        loginPage.clickLoginButton();
+        Assertions.assertEquals(LoginMessage.EMPTY_PHONE_NUMBER_FIELD_MESSAGE,loginPage.getErrorMessage());
+    }
 }
